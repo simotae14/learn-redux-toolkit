18aada8b8308ca0779001934f0f14dd28fd2f0ac
Simone Taeggi <startae14@gmail.com>
2023-11-15 11:23:52 +0100

    define first implementation of the api with rtk

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,39 @@ import { store } from './app/store'
 import App from './App.tsx'
 import './index.css'
 
+import { fakePokemonDetailData, fakePokemonListing } from './utils/fakeData.ts'
+import { createApi, ApiProvider } from '@reduxjs/toolkit/query/react'
+
+const api = createApi({
+  baseQuery: () => {},
+  endpoints: (builder) => ({
+    pokemonList: builder.query({
+      queryFn() {
+        return {
+          data: fakePokemonListing,
+        };
+      }
+    }),
+    pokemonDetail: builder.query({
+      queryFn() {
+        return {
+          data: fakePokemonDetailData,
+        };
+      }
+    }),
+  }),
+});
+
+// it creates two hooks for us
+const { usePokemonListQuery, usePokemonDetailQuery } = api;
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApiProvider api={api}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApiProvider>
   </React.StrictMode>,
 )
